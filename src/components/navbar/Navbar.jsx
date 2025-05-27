afcfae159996d1c1e78adfd153c4a4934a54febd
jaumecf <caf@paucasesnovescifp.cat>
2025-05-27 23:39:56 +0200

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+        <ul>
+        <li className="titol"><Link to="/">Despesapp</Link></li>
+        <li><Link to="/">Inici</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        </ul>
+    </nav>
+  )
+}
