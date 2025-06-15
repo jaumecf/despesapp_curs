cfffa31da2124de7639ab88f07622f6913cf74f1
jaumecf <caf@paucasesnovescifp.cat>
2025-06-15 19:16:55 +0200

    config uploaded

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+apiKey: "AIzaSyAKsGIRWxBaqAe3wJIcmwPuU0s6cXRIoxY",
+authDomain: "despesapp-curs-fd40a.firebaseapp.com",
+projectId: "despesapp-curs-fd40a",
+storageBucket: "despesapp-curs-fd40a.firebasestorage.app",
+messagingSenderId: "988133699578",
+appId: "1:988133699578:web:b6c22ac511f922b7f2fe43"
+};
